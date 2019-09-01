e33d7f231579895ab44c54eaa09972b4704697e5
devheedoo <devheedoo@gmail.com>
2019-09-02 00:52:26 +0900

    멀티 카운터 생성

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-// Presentational Component
-const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
+const Counter = ({ number, color, index, onIncrement, onDecrement, onSetColor }) => {
   return (
     <div
       className="Counter"
-      onClick={onIncrement}
+      onClick={() => onIncrement(index)}
       onContextMenu={(e) => {
         e.preventDefault();
-        onDecrement();
+        onDecrement(index);
       }}
-      onDoubleClick={onSetColor}
+      onDoubleClick={() => onSetColor(index)}
       style={{
         backgroundColor: color
       }}
@@ -25,6 +24,7 @@ const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
 Counter.propTypes = {
   number: PropTypes.number,
   color: PropTypes.string,
+  index: PropTypes.number,
   onIncrement: PropTypes.func,
   onDecrement: PropTypes.func,
   onSetColor: PropTypes.func
@@ -33,6 +33,7 @@ Counter.propTypes = {
 Counter.defaultProps = {
   number: 0,
   color: 'black',
+  index: 0,
   onIncrement: () => console.warn('onIncrement not defined'),
   onDecrement: () => console.warn('onDecrement not defined'),
   onSetColor: () => console.warn('onSetColor not defined')
